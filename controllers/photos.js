00e5a33badbe95c965579e469711079289ce807c
jaures-appia <47213767+jaures-appia@users.noreply.github.com>
2020-09-16 12:10:47 +0000

    terminated crud of photo

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,19 +1,9 @@
 const Photo = require('../models/Photo')
+const fs = require('fs')
 
 exports.addPhoto = (req, res, next) => {
-    console.log(req.body)
-    // const photo = new Photo({
-    //     title: req.body.title,
-    //     photoUrl: req.body.photoUrl,
-    //     description: req.body.description,
-    //     UserId: req.body.UserId,
-    // });
-    // photo.save()
-    //     .then((photo) => res.status(201).json({ photo: photo }))
-    //     .catch(error => res.status(401).json({ error }));
-
-    // const photoObject = req.body.photo
-    // delete photoObject._id;
+    // console.log(req.body, req.file)
+    
     const photo = new Photo({
         title: req.body.title,
         description: req.body.description,
@@ -22,9 +12,7 @@ exports.addPhoto = (req, res, next) => {
     });
     photo.save()
         .then((photo) => res.status(201).json({ photo: photo }))
-        .catch(error => res.status(401).json({ error }));
-        
-    
+        .catch(error => res.status(401).json({ error })); 
 }
 
 exports.getAllPhotos = (req, res, next) => {
@@ -44,36 +32,39 @@ exports.getOnePhoto = (req, res, next) => {
 }
 
 exports.modifyPhoto = (req, res, next) => {
-    const photo = new Photo({
+  // console.log(req.body)
+  // console.log(req.file)
+  const photo = req.file ?
+     new Photo({
         _id: req.params.id,
         title: req.body.title,
-        photoUrl: req.body.photoUrl,
+        photoUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         description: req.body.description,
         UserId: req.body.UserId,
     })
-    Photo.updateOne({_id: req.params.id}, photo)
-        .then(() => {
-            res.status(201).json({
-            message: 'Photo updated successfully!'
-            });
-        })
-        .catch((error) => {
-            res.status(400).json({
-            error: error
-            });
-        })
+     : new Photo({
+      _id: req.params.id,
+      title: req.body.title,
+      photoUrl: req.body.photoUrl,
+      description: req.body.description,
+      UserId: req.body.UserId,
+  })
+  Photo.updateOne({ _id: req.params.id }, photo)
+    .then(() => res.status(201).json({ message: 'photo modifié !'}))
+    .catch(error => res.status(401).json({ error }));
 }
 
 exports.deletePhoto = (req, res, next) => {
-    Photo.deleteOne({_id: req.params.id})
-    .then(() => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch((error) => {
-        res.status(400).json({
-          error: error
-        })
-      })
+  Photo.findOne({ _id: req.params.id })
+    .then(photo => {
+      // console.log(photo)
+      const filename = photo.photoUrl.split('/images/')[1];
+      // console.log(photo.photoUrl.split('/images/'))
+      fs.unlink(`images/${filename}`, () => {
+        Photo.deleteOne({ _id: req.params.id })
+          .then(() => res.status(201).json({ message: 'Objet supprimé !'}))
+          .catch(error => res.status(401).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
 }
--- routes/photos.js
+++ routes/photos.js
@@ -7,7 +7,7 @@ const photoCtrl = require('../controllers/photos')
 router.post('/', multer, photoCtrl.addPhoto);
 router.get('/', photoCtrl.getAllPhotos);
 router.get('/:id', photoCtrl.getOnePhoto);
-router.put('/:id', photoCtrl.modifyPhoto);
+router.put('/:id', multer, photoCtrl.modifyPhoto);
 router.delete('/:id', photoCtrl.deletePhoto);
 
 module.exports = router;
